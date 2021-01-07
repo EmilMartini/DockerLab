fbb97bbe884f27d27bee069d9c96dea014dde620
emil.martini <emil.martini@contica.se>
2021-01-07 10:40:19 +0100

    add databaseclient

---
+++ BugReporter/BugReporter.Database/Database/DatabaseClient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugReporter.Database
+{
+    public class DatabaseClient
+    {
+        private string localPath;
+        private string currentDir;
+        private string fileName;
+
+        public DatabaseClient()
+        {
+            currentDir = Environment.CurrentDirectory;
+        }
+
+        public void ReadFromFile()
+        {
+            //Read file and convert to json
+        }
+
+        public void SaveToFile()
+        {
+            //Save and overwrite current file
+        }  
+    }
+}
