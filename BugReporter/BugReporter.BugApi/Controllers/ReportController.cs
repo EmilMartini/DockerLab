22ae01a7a1f8566f3cf862dc8dc35c4c444b5ca7
emil.martini <emil.martini@contica.se>
2021-01-09 09:39:01 +0100

    add API call in BugApi

--- BugReporter/BugReporter.BugApi/Controllers/ReportController.cs
+++ BugReporter/BugReporter.BugApi/Controllers/ReportController.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BugReporter.BugApi.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace BugReporter.BugApi.Controllers
@@ -11,6 +15,53 @@ namespace BugReporter.BugApi.Controllers
     [ApiController]
     public class ReportController : ControllerBase
     {
+        Random rnd;
+        HttpClient client;
+        string baseUrl;
+
+        public ReportController()
+        {
+            client = new HttpClient();
+            rnd = new Random();
+            baseUrl = "http://localhost:1337/api/Database";
+        }
+
+        [HttpPost]
+        [Route("Bug")]
+        public IActionResult CreateBug(string description)
+        {
+            var bug = new Bug()
+            {
+                Id = rnd.Next(0, int.MaxValue),
+                Description = description
+            };
+
+            var response = client.GetAsync(baseUrl + "/File").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var bugs = JsonConvert.DeserializeObject<List<Bug>>(response.Content.ReadAsStringAsync().Result);
+
+                while(bugs.Where(i => i.Id == bug.Id).ToList().Count > 0)
+                {
+                    bug.Id = rnd.Next(0, int.MaxValue);
+                }
+
+                bugs.Add(bug);
+
+                var httpContent = new HttpRequestMessage(HttpMethod.Post, baseUrl + "/File");
+                httpContent.Headers.Add("content-type", "application/json");
+                httpContent.Content = new StringContent(JsonConvert.SerializeObject(bugs), Encoding.UTF8, "application/json");
+
+                response = client.SendAsync(httpContent).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return Ok();
+                }
+            }
+
+            return BadRequest();
+        }
     }
 
     //ta in string skapa en bug, skicka till DB
